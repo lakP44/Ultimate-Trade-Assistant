54a99701a49b5dc0032819f9518b38d54caac320
lakP44 <laklak5000@gmail.com>
2024-09-28 21:38:20 +0900

    가상환경 생성 및 requirements 생성
    예제코드에서 사용하는 모듈 일단 import

---
+++ main.py
@@ -0,0 +1,16 @@
+import API
+import requests
+import json
+import datetime
+import time
+
+# python -m venv 가상환경이름 <== 가상환경 생성
+# cd stockk\Scripts
+# activate.bat
+# deactivate
+
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
+
+test = API.id
+print(test)
